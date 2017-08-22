61c05f9f4b6aba72da3f2e28fc4beca52edae35c
Gilbert Gwizdała <ggwizdala@sointeractive.pl>
2017-08-22 14:23:42 +0200

    Add storm

--- AR3D/ViewController.swift
+++ AR3D/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         
+        
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
         self.view.addGestureRecognizer(tapGesture)
     }
@@ -42,20 +43,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let tapPoint = sender.location(in: self.view)
             let viewSize = self.view.frame.size
             let testPoint = CGPoint(x: tapPoint.x / viewSize.width, y: tapPoint.y / viewSize.height)
-            let hits = self.sceneView.hitTest(testPoint, types: [.existingPlaneUsingExtent, .featurePoint])
-            if let hit = hits.first {
-                print(hit.localTransform)
-                //let node = SCNNode(geometry: SCNBox(width: 10, height: 10, length: 10, chamferRadius: 10))
-                let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
-                let position = hit.worldTransform.position()
-                    cubeNode.position = position
-                    self.sceneView.scene.rootNode.addChildNode(cubeNode)
-                
-                
-                
+            //let hits = self.sceneView.hitTest(testPoint, types: [.existingPlaneUsingExtent, .featurePoint])
+            let hits = self.sceneView.session.currentFrame?.hitTest(testPoint, types: [.existingPlaneUsingExtent, .featurePoint]) ?? []
+            print(hits.count)
+            if let hit = hits.first, let anchor = hit.anchor, let node = self.sceneView.node(for: anchor) {
+ 
+                if let s = loadNode() {
+                    s.position = SCNVector3(x: 0, y: 0, z: 0)
+                    node.addChildNode(s)
+                }
                 
             }
+        
         }
+    }
+    
+    func loadNode() -> SCNNode? {
+        
+        let scene = SCNScene(named: "art.scnassets/Stormtrooper.scn")
+        let node = scene?.rootNode
+        node?.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+        node?.scale = SCNVector3(0.5, 0.5, 0.5)
+        return node
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -85,11 +94,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // MARK: - ARSCNViewDelegate
     
 
-    // Override to create and configure nodes for anchors added to the view's session.
+//     Override to create and configure nodes for anchors added to the view's session.
 //    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-//        let node = SCNNode()
-//
-//        return node
+//        guard let planeAncher = anchor as? ARPlaneAnchor else { return nil}
+//        let node = self.sceneView.node(for: planeAncher)
+//        return nil
 //    }
     
     
@@ -97,20 +106,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         // This visualization covers only detected planes.
         
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        if let planeAnchor = anchor as? ARPlaneAnchor {
+            
+            print(planeAnchor)
         
-        // Create a SceneKit plane to visualize the node using its position and extent.
-        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-        let planeNode = SCNNode(geometry: plane)
-        planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
-        
-        // SCNPlanes are vertically oriented in their local coordinate space.
-        // Rotate it to match the horizontal orientation of the ARPlaneAnchor.
-        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
-        
-        // ARKit owns the node corresponding to the anchor, so make the plane a child node.
-        node.addChildNode(planeNode)
-        
+            // Create a SceneKit plane to visualize the node using its position and extent.
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let mat = SCNMaterial()
+            mat.diffuse.contents = UIImage(named:"grid")
+            mat.locksAmbientWithDiffuse = true
+            plane.materials = [mat]
+            let planeNode = SCNNode(geometry: plane)
+            print("add node")
+            planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
+            
+            // SCNPlanes are vertically oriented in their local coordinate space.
+            // Rotate it to match the horizontal orientation of the ARPlaneAnchor.
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+            
+            // ARKit owns the node corresponding to the anchor, so make the plane a child node.
+            node.addChildNode(planeNode)
+            node.name = "planeNode"
+        }
         
     }
 
@@ -131,8 +148,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 }
 
-extension matrix_float4x4 {
-    func position() -> SCNVector3 {
-        return SCNVector3(columns.3.x, columns.3.y, columns.3.z)
-    }
-}
+
